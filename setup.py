c9c0b55acf4e3a848303431eb7aef6ce7b9650bc
Michael <info@meine-buybox.de>
2020-12-18 22:37:44 +0100

    initial commit

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.0.1',
     packages=['tests', 'tests.api', 'tests.api.orders', 'tests.api.sellers', 'tests.api.finances',
               'tests.api.product_fees', 'tests.api.notifications', 'tests.client', 'sp_api', 'sp_api.api',
-              'sp_api.auth', 'sp_api.base'],
+              'sp_api.api.finances', 'sp_api.auth', 'sp_api.base'],
     url='https://github.com/saleweaver/python-sp-api',
     license='MIT',
     author='Michael Primke',
