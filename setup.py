ff98080a785e1efe05fcebb18fca02b920b1a358
Michael <info@meine-buybox.de>
2020-12-18 23:04:20 +0100

    initial commit

--- setup.py
+++ setup.py
@@ -6,6 +6,8 @@ setup(
     packages=['tests', 'tests.api', 'tests.api.orders', 'tests.api.sellers', 'tests.api.finances',
               'tests.api.product_fees', 'tests.api.notifications', 'tests.client', 'sp_api', 'sp_api.api',
               'sp_api.api.orders', 'sp_api.api.orders.models', 'sp_api.api.sellers', 'sp_api.api.finances',
+              'sp_api.api.finances.models', 'sp_api.api.sellers.models', 'sp_api.api.notifications.models',
+              'sp_api.api.product_fees.models',
               'sp_api.api.product_fees', 'sp_api.api.notifications', 'sp_api.auth', 'sp_api.base'],
     url='https://github.com/saleweaver/python-sp-api',
     license='MIT',
