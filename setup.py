48301176bcfa36e37cf1412b595334626dcf5998
Michael <info@meine-buybox.de>
2020-12-18 23:06:33 +0100

    initial commit

--- setup.py
+++ setup.py
@@ -3,6 +3,14 @@ from setuptools import setup
 setup(
     name='python-sp-api',
     version='0.0.1',
+    requires=[
+        "pytest",
+        "requests~=2.25.1",
+        "six~=1.15.0",
+        "boto3~=1.16.39",
+        "cachetools~=4.2.0"
+
+    ],
     packages=['tests', 'tests.api', 'tests.api.orders', 'tests.api.sellers', 'tests.api.finances',
               'tests.api.product_fees', 'tests.api.notifications', 'tests.client', 'sp_api', 'sp_api.api',
               'sp_api.api.orders', 'sp_api.api.orders.models', 'sp_api.api.sellers', 'sp_api.api.finances',
