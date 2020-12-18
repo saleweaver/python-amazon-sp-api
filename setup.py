d04607674ebd9de06451d5f082a82d7a0d462299
Michael <info@meine-buybox.de>
2020-12-18 22:29:50 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='python-sp-api',
+    version='0.0.1',
+    packages=['tests', 'tests.api', 'tests.api.orders', 'tests.api.sellers', 'tests.api.finances',
+              'tests.api.product_fees', 'tests.api.notifications', 'tests.client', 'sp_api', 'sp_api.api',
+              'sp_api.auth', 'sp_api.base'],
+    url='https://github.com/saleweaver/python-sp-api',
+    license='MIT',
+    author='Michael Primke',
+    author_email='info@saleweaver.com',
+    description='Python wrapper to access the amazon selling partner API'
+)
