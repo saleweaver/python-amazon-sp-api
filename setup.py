52f11beacbbb660cfab15e52b58d070c7a62e915
Michael <info@meine-buybox.de>
2020-12-22 20:59:06 +0100

    added product fees

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='python-amazon-sp-api',
-    version='0.0.3',
+    version='0.0.4',
     install_requires=[
         "requests",
         "six~=1.15.0",
