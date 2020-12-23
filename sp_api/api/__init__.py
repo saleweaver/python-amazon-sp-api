192a3dfdcc38e26bd81872c2db77f3d3f17cee28
Michael <info@meine-buybox.de>
2020-12-23 21:38:32 +0100

    fixed

--- sp_api/api/__init__.py
+++ sp_api/api/__init__.py
@@ -4,8 +4,10 @@ from .orders.orders import Orders
 from .product_fees.product_fees import ProductFees
 from .sellers.sellers import Sellers
 from .reports.reports import Reports
+from .products.products import Products
 
 __all__ = [
+    "Products",
     "Reports",
     "Orders",
     "Sellers",
