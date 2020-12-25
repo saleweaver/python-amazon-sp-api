2fa93364fd17501d379c73b8935b8a97210d0a79
Michael <info@meine-buybox.de>
2020-12-25 17:29:37 +0100

    fixed

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='python-amazon-sp-api',
-    version='0.0.10',
+    version='0.0.11',
     install_requires=[
         "requests",
         "six~=1.15.0",
