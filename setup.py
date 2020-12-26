b32b1f840c7e88e937609e41ede2d6da0691ef8b
Michael <info@meine-buybox.de>
2020-12-26 21:25:18 +0100

    fixed space in path

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='python-amazon-sp-api',
-    version='0.0.11',
+    version='0.0.12',
     install_requires=[
         "requests",
         "six~=1.15.0",
