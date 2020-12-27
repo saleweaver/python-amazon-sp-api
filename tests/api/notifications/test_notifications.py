431b0e917c9d46f4328e8021308a6dfd460c3332
Michael <info@saleweaver.com>
2020-12-27 22:31:52 +0100

    added Sales endpoint

--- tests/api/notifications/test_notifications.py
+++ tests/api/notifications/test_notifications.py
@@ -4,7 +4,7 @@ from sp_api.base import SellingApiException
 
 def test_create_destination():
     try:
-        res = Notifications().create_destination(name='test', arn='ad')
+        res = Notifications().create_destination(name='test', arn='')
         print(res)
     except SellingApiException as e:
         print(e)
