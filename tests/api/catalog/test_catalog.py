354ce7f8b49329f245d968633597a628366f8a5c
Michael <info@saleweaver.com>
2021-01-28 10:25:31 +0100

    setup

--- tests/api/catalog/test_catalog.py
+++ tests/api/catalog/test_catalog.py
@@ -3,7 +3,7 @@ from sp_api.base import Marketplaces
 
 
 def test_catalog_get_item():
-    asin = 'B008DZ2JT4'
-    print(Catalog(marketplace=Marketplaces.DE).get_item(asin))
-    # print(Catalog(marketplace=Marketplaces.DE).list_items(EAN='0730143302517'))
+    asin = ''
+    print(Catalog().get_item(asin))
+    # print(Catalog(marketplace=Marketplaces.DE).list_items(EAN=''))
 
--- tests/api/products/test_products.py
+++ tests/api/products/test_products.py
@@ -1,7 +1,8 @@
 import urllib
 
 from sp_api.api.products.products import Products
+from sp_api.base import Marketplaces
 
 
 def test_pricing_for_sku():
-    print(Products().get_product_pricing_for_skus([]))
+    print(Products().get_product_pricing_for_skus(['abc']))
