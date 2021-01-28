9f80e50643d07d857962ea5648efca364daf4df3
Lee Field <lee.field@gmail.com>
2021-01-28 20:13:59 +0000

    Added competitivePrice endpoint Altered price endpoint to include ItemCondition for ItemType Asin

--- sp_api/api/products/products.py
+++ sp_api/api/products/products.py
@@ -8,6 +8,7 @@ class Products(Client):
     def __init__(self, marketplace=Marketplaces.US, *, refresh_token=None, account='default', credentials=None):
         super().__init__(marketplace, refresh_token, account, credentials)
 
+    @sp_endpoint('/products/pricing/v0/price', method='GET')
     def get_product_pricing_for_skus(self, seller_sku_list, **kwargs):
         """
         Returns pricing information for a seller's offer listings based on seller sku.
@@ -18,17 +19,50 @@ class Products(Client):
         """
         return self._create_get_pricing_request(seller_sku_list, 'Sku', **kwargs)
 
-    def get_product_pricing_for_asins(self, asin_list, **kwargs):
+    @sp_endpoint('/products/pricing/v0/price', method='GET')
+    def get_product_pricing_for_asins(self, asin_list, item_condition=None, **kwargs):
         """
         Returns pricing information for a seller's offer listings based on ASIN.
 
         :param asin_list:
+        :param item_condition:
+           Filters the offer listings based on item condition. Possible values: New, Used, Collectible, Refurbished, Club.
+           Available values : New, Used, Collectible, Refurbished, Club
         :param kwargs:
         :return:
         """
+        if item_condition is not None:
+            kwargs['ItemCondition'] = item_condition
+
         return self._create_get_pricing_request(asin_list, 'Asin', **kwargs)
 
-    @sp_endpoint('/products/pricing/v0/price', method='GET')
+
+    @sp_endpoint('/products/pricing/v0/competitivePrice', method='GET')
+    def get_competitive_pricing_for_skus(self, seller_sku_list, **kwargs):
+        """
+        Returns competitive pricing information for a seller's offer listings based on Seller Sku
+        :param asin_list:
+        :param kwargs:
+        :return:
+
+
+        """
+        return self._create_get_pricing_request(seller_sku_list, 'Sku', **kwargs)
+
+
+    @sp_endpoint('/products/pricing/v0/competitivePrice', method='GET')
+    def get_competitive_pricing_for_asins(self, asin_list, **kwargs):
+        """
+        Returns competitive pricing information for a seller's offer listings based on ASIN
+        :param asin_list:
+        :param kwargs:
+        :return:
+
+
+        """
+        return self._create_get_pricing_request(asin_list, 'Asin', **kwargs)
+
+
     def _create_get_pricing_request(self, item_list, item_type, **kwargs):
         """
         Returns pricing information for a seller's offer listings based on seller SKU or ASIN.
@@ -51,4 +85,5 @@ class Products(Client):
                             params={**{f"{item_type}s": ','.join(
                                 [urllib.parse.quote_plus(s) for s in item_list])},
                                     'ItemType': item_type,
+                                    **({'ItemCondition': kwargs.pop('ItemCondition')} if 'ItemCondition' in kwargs else {}),
                                     'MarketplaceId': self.marketplace_id}).json())
