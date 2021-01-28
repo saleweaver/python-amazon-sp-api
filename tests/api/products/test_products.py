5931250402d3165a0e4104dc9ec7414c6968a016
Michael <info@saleweaver.com>
2021-01-28 21:55:25 +0100

    added docs

--- tests/api/products/test_products.py
+++ tests/api/products/test_products.py
@@ -5,4 +5,4 @@ from sp_api.base import Marketplaces
 
 
 def test_pricing_for_sku():
-    print(Products().get_product_pricing_for_skus(['abc']))
+    print(Products().get_competitive_pricing_for_asins(['']))
