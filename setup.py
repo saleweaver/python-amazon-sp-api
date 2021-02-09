8cc123b6b57ea2d00475e05f579d8db21fbd7dea
Michael <info@saleweaver.com>
2021-02-09 17:53:44 +0100

    fixed bug for points

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='python-amazon-sp-api',
-    version='0.2.2',
+    version='0.2.3',
     install_requires=[
         "requests",
         "six~=1.15.0",
