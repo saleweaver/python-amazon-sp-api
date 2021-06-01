babb6c31b794a80acee13863a07c0154095f05cd
Michael <info@saleweaver.com>
2021-06-01 15:50:13 +0200

    access token client in base

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='python-amazon-sp-api',
-    version='0.4.6',
+    version='0.4.7',
     install_requires=[
         "requests",
         "six~=1.15.0",
