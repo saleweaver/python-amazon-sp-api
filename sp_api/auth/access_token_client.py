fc5c7a99a41f0cd8ebbde0208264545008845131
Jack Holkeboer <jack.holkeboer@outlook.com>
2021-07-25 14:26:51 -0400

    fix typo

--- sp_api/auth/access_token_client.py
+++ sp_api/auth/access_token_client.py
@@ -142,7 +142,7 @@ class AccessTokenClient(BaseClient):
 
     def _get_cache_key(self, token_flavor=''):
         return 'access_token_' + hashlib.md5(
-            (token_flavor + self.cred.refresh_token).encode('utf-8')        \
+            (token_flavor + self.cred.refresh_token).encode('utf-8')
         ).hexdigest()
 
     def get_secret(self):
