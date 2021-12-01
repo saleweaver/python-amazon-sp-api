b5ec0064b48789cbfb1521495a469e11f0aa9a55
Michael <info@saleweaver.com>
2021-12-01 02:42:04 +0100

    fixed docs

--- sp_api/api/services/services.py
+++ sp_api/api/services/services.py
@@ -189,7 +189,7 @@ class Services(Client):
         Args:
             serviceJobId:string | * REQUIRED An Amazon defined service job identifier.
             appointmentId:string | * REQUIRED An existing appointment identifier for the Service Job.
-            kwargs: Example |
+            kwargs: Example | >>>
                 {
                     "appointmentTime": {
                         "startTime": "2019-08-24T14:15:22Z",
