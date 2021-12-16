b3ce4f76077b86347f1f3565182c7593ac684348
Michael <info@saleweaver.com>
2021-12-17 00:43:10 +0100

    some fixes

--- sp_api/api/feeds/feeds_v2.py
+++ sp_api/api/feeds/feeds_v2.py
@@ -21,37 +21,55 @@ class FeedsV2(Client):
 
         Returns feed details for the feeds that match the filters that you specify.
 
-**Usage Plan:**
-
-| Rate (requests per second) | Burst |
-| ---- | ---- |
-| 0.0222 | 10 |
-
-For more information, see "Usage Plans and Rate Limits" in the Selling Partner API documentation.
-
-         Args:
-        
+        Args:
             key feedTypes:array |  A list of feed types used to filter feeds. When feedTypes is provided, the other filter parameters (processingStatuses, marketplaceIds, createdSince, createdUntil) and pageSize may also be provided. Either feedTypes or nextToken is required.
-        
             key marketplaceIds:array |  A list of marketplace identifiers used to filter feeds. The feeds returned will match at least one of the marketplaces that you specify.
-        
             key pageSize:integer |  The maximum number of feeds to return in a single call.
-        
             key processingStatuses:array |  A list of processing statuses used to filter feeds.
-        
             key createdSince:string |  The earliest feed creation date and time for feeds included in the response, in ISO 8601 format. The default is 90 days ago. Feeds are retained for a maximum of 90 days.
-        
             key createdUntil:string |  The latest feed creation date and time for feeds included in the response, in ISO 8601 format. The default is now.
-        
             key nextToken:string |  A string token returned in the response to your previous request. nextToken is returned when the number of results exceeds the specified pageSize value. To get the next page of results, call the getFeeds operation and include this token as the only parameter. Specifying nextToken with any other parameters will cause the request to fail.
         
 
-         Returns:
+        Returns:
             ApiResponse:
         """
     
         return self._request(kwargs.pop('path'),  params=kwargs)
     
+    def submit_feed(self, feed_type, file, content_type='text/tsv', **kwargs) -> [ApiResponse, ApiResponse]:
+        """
+        submit_feed(self, feed_type: str, file: File or File like, content_type='text/tsv', **kwargs) -> [ApiResponse, ApiResponse]
+        Combines `create_feed_document` and `create_feed`, uploads the file and sends the feed to amazon.
+
+        **Usage Plan:**
+
+        ======================================  ==============
+        Rate (requests per second)               Burst
+        ======================================  ==============
+        0.0083                                  15
+        ======================================  ==============
+
+        Examples:
+            literal blocks::
+
+                feed = BytesIO
+                feed.write(<your feed>)
+                feed.seek(0)
+                Feeds().submit_feed('POST_FBA_INBOUND_CARTON_CONTENTS', feed, 'text/xml')
+
+
+        Args:
+            feed_type:
+            file:
+            content_type:
+            **kwargs:
+
+        Returns:
+            [CreateFeedDocumentResponse, CreateFeedResponse]:
+        """
+        document_response = self.create_feed_document(file, content_type)
+        return document_response, self.create_feed(feed_type, document_response.payload.get('feedDocumentId'), **kwargs)
 
     @sp_endpoint('/feeds/2021-06-30/feeds', method='POST')
     def create_feed(self, feed_type, input_feed_document_id, **kwargs) -> ApiResponse:
@@ -100,20 +118,12 @@ For more information, see "Usage Plans and Rate Limits" in the Selling Partner A
 
         Cancels the feed that you specify. Only feeds with processingStatus=IN_QUEUE can be cancelled. Cancelled feeds are returned in subsequent calls to the getFeed and getFeeds operations.
 
-**Usage Plan:**
-
-| Rate (requests per second) | Burst |
-| ---- | ---- |
-| 0.0222 | 10 |
-
-For more information, see "Usage Plans and Rate Limits" in the Selling Partner API documentation.
-
-         Args:
+        Args:
         
             feedId:string | * REQUIRED The identifier for the feed. This identifier is unique only in combination with a seller ID.
         
 
-         Returns:
+        Returns:
             ApiResponse:
         """
     
@@ -127,20 +137,12 @@ For more information, see "Usage Plans and Rate Limits" in the Selling Partner A
 
         Returns feed details (including the resultDocumentId, if available) for the feed that you specify.
 
-**Usage Plan:**
-
-| Rate (requests per second) | Burst |
-| ---- | ---- |
-| 2.0 | 15 |
-
-For more information, see "Usage Plans and Rate Limits" in the Selling Partner API documentation.
-
-         Args:
+        Args:
         
             feedId:string | * REQUIRED The identifier for the feed. This identifier is unique only in combination with a seller ID.
         
 
-         Returns:
+        Returns:
             ApiResponse:
         """
     
@@ -153,12 +155,6 @@ For more information, see "Usage Plans and Rate Limits" in the Selling Partner A
         create_feed_document(self, **kwargs) -> ApiResponse
 
         Creates a feed document for the feed type that you specify. This operation returns a presigned URL for uploading the feed document contents. It also returns a feedDocumentId value that you can pass in with a subsequent call to the createFeed operation.
-
-        **Usage Plan:**
-
-        | Rate (requests per second) | Burst |
-        | ---- | ---- |
-        | 0.0083 | 15 |
 
         For more information, see "Usage Plans and Rate Limits" in the Selling Partner API documentation.
 
@@ -192,55 +188,16 @@ For more information, see "Usage Plans and Rate Limits" in the Selling Partner A
 
         Returns the information required for retrieving a feed document's contents.
 
-**Usage Plan:**
+        For more information, see "Usage Plans and Rate Limits" in the Selling Partner API documentation.
 
-| Rate (requests per second) | Burst |
-| ---- | ---- |
-| 0.0222 | 10 |
-
-For more information, see "Usage Plans and Rate Limits" in the Selling Partner API documentation.
-
-         Args:
+        Args:
         
             feedDocumentId:string | * REQUIRED The identifier of the feed document.
         
 
-         Returns:
+        Returns:
             ApiResponse:
         """
     
         return self._request(fill_query_params(kwargs.pop('path'), feedDocumentId), params=kwargs)
     
-    def submit_feed(self, feed_type, file, content_type='text/tsv', **kwargs) -> [ApiResponse, ApiResponse]:
-        """
-        submit_feed(self, feed_type: str, file: File or File like, content_type='text/tsv', **kwargs) -> [ApiResponse, ApiResponse]
-        Combines `create_feed_document` and `create_feed`, uploads the encrypted file and sends the feed to amazon.
-
-        **Usage Plan:**
-
-        ======================================  ==============
-        Rate (requests per second)               Burst
-        ======================================  ==============
-        0.0083                                  15
-        ======================================  ==============
-
-        Examples:
-            literal blocks::
-
-                feed = BytesIO
-                feed.write(<your feed>)
-                feed.seek(0)
-                Feeds().submit_feed('POST_FBA_INBOUND_CARTON_CONTENTS', feed, 'text/xml')
-
-
-        Args:
-            feed_type:
-            file:
-            content_type:
-            **kwargs:
-
-        Returns:
-            [CreateFeedDocumentResponse, CreateFeedResponse]:
-        """
-        document_response = self.create_feed_document(file, content_type)
-        return document_response, self.create_feed(feed_type, document_response.payload.get('feedDocumentId'), **kwargs)
