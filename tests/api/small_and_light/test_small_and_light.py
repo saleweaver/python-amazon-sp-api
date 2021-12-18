838e8a7ab40603ad5cfa6efcdf180eb16c8c41c8
Michael <info@saleweaver.com>
2021-12-18 22:39:29 +0100

    added tests

--- tests/api/small_and_light/test_small_and_light.py
+++ tests/api/small_and_light/test_small_and_light.py
@@ -34,5 +34,5 @@ def test_get_small_and_light_enrollment_by_seller_sku():
 
 
 def test_put_small_and_light_enrollment_by_seller_sku():
-    res = FbaSmallAndLight().put_small_and_light_enrollment_by_seller_sku('SKU_ELIGIBLE_FOR_SMALL_AND_LIGHT', marketplaceIds='ATVPDKIKX0DER')
+    res = FbaSmallAndLight().put_small_and_light_enrollment_by_seller_sku('SKU_ELIGIBLE_FOR_SMALL_AND_LIGHT')
     assert res() is not None
