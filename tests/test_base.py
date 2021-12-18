6479d1577236309d2266a2651c58a50491546e41
Michael <info@saleweaver.com>
2021-12-18 23:19:18 +0100

    added tests

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,5 +1,17 @@
+import os
+
 from sp_api.api import FulfillmentInbound
-from sp_api.base import Marketplaces
+from sp_api.base import Marketplaces, MissingCredentials
+from sp_api.base.credential_provider import FromCodeCredentialProvider, FromEnvironmentVariablesCredentialProvider, \
+    FromSecretsCredentialProvider, FromConfigFileCredentialProvider
+
+
+refresh_token = '<refresh_token>'
+lwa_app_id = '<lwa_app_id>'
+lwa_client_secret = '<lwa_client_secret>'
+aws_secret_key = '<aws_secret_access_key>'
+aws_access_key = '<aws_access_key_id>'
+role_arn = '<role_arn>'
 
 
 def test_api_response_has_next_token():
@@ -11,3 +23,68 @@ def test_marketplaces():
     assert Marketplaces.DE.region == 'eu-west-1'
     assert Marketplaces.US.marketplace_id == 'ATVPDKIKX0DER'
 
+
+def test_from_code_credential_provider():
+    p = FromCodeCredentialProvider(credentials=dict(
+        refresh_token=refresh_token,
+        lwa_app_id=lwa_app_id,
+        lwa_client_secret=lwa_client_secret,
+        aws_secret_key=aws_secret_key,
+        aws_access_key=aws_access_key,
+        role_arn=role_arn,
+    ))
+    assert p.credentials is not None
+    assert isinstance(p.credentials, dict)
+
+
+def test_from_code_credential_provider_no_role():
+    p = FromCodeCredentialProvider(credentials=dict(
+        refresh_token=refresh_token,
+        lwa_app_id=lwa_app_id,
+        lwa_client_secret=lwa_client_secret,
+        aws_secret_key=aws_secret_key,
+        aws_access_key=aws_access_key,
+    ))
+    assert p.credentials is not None
+    assert isinstance(p.credentials, dict)
+    assert p.credentials.get('role_arn') is None
+
+
+def test_from_code_credential_provider_no_role_no_refresh_token():
+    p = FromCodeCredentialProvider(credentials=dict(
+        lwa_app_id=lwa_app_id,
+        lwa_client_secret=lwa_client_secret,
+        aws_secret_key=aws_secret_key,
+        aws_access_key=aws_access_key,
+    ))
+    assert p.credentials is not None
+    assert isinstance(p.credentials, dict)
+    assert p.credentials.get('role_arn') is None
+    assert p.credentials.get('refresh_token') is None
+
+
+def test_env_vars_provider():
+    os.environ['SP_API_REFRESH_TOKEN'] = 'foo'
+    os.environ['LWA_APP_ID'] = 'foo'
+    os.environ['LWA_CLIENT_SECRET'] = 'foo'
+    os.environ['SP_API_ACCESS_KEY'] = 'foo'
+    os.environ['SP_API_SECRET_KEY'] = 'foo'
+    os.environ['SP_API_ROLE_ARN'] = 'foo'
+
+    p = FromEnvironmentVariablesCredentialProvider()()
+    assert 'refresh_token' in p
+
+
+def test_from_secrets():
+    os.environ['SP_API_AWS_SECRET_ID'] = 'testing/sp-api-foo'
+    p = FromSecretsCredentialProvider()()
+    assert 'refresh_token' in p
+    assert p.get('refresh_token') == 'foo'
+
+
+def test_from_config_file_provider():
+    try:
+        p = FromConfigFileCredentialProvider()()
+        assert p.get('refresh_token') is not None
+    except MissingCredentials as e:
+        assert isinstance(e, MissingCredentials)
