70d2d4206911c3ace5861c3d9f61d43d24c9e6ec
Michael <info@saleweaver.com>
2021-12-19 23:49:04 +0100

    added tests

--- tests/client/test_auth.py
+++ tests/client/test_auth.py
@@ -1,6 +1,14 @@
 from sp_api.base import AccessTokenClient
 from sp_api.base import Credentials, CredentialProvider
 from sp_api.base import AuthorizationError
+from sp_api.base.credential_provider import FromCodeCredentialProvider
+refresh_token = '<refresh_token>'
+lwa_app_id = '<lwa_app_id>'
+lwa_client_secret = '<lwa_client_secret>'
+aws_secret_key = '<aws_secret_access_key>'
+aws_access_key = '<aws_access_key_id>'
+role_arn = '<role_arn>'
+
 
 
 def test_auth_exception():
@@ -19,7 +27,14 @@ def test_credentials():
 
 
 def test_auth_client():
-    client = AccessTokenClient(credentials=CredentialProvider().credentials)
+    client = AccessTokenClient(credentials=CredentialProvider(credentials=dict(
+        refresh_token=refresh_token,
+        lwa_app_id=lwa_app_id,
+        lwa_client_secret=lwa_client_secret,
+        aws_secret_key=aws_secret_key,
+        aws_access_key=aws_access_key,
+        role_arn=role_arn,
+    )).credentials)
     x = client._auth_code_request_body('foo')
     assert x.get('grant_type') == 'authorization_code'
 
