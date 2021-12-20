11e5a2474c1ed97e1acd3488b0d81d30a26e552f
Michael <info@saleweaver.com>
2021-12-20 20:39:22 +0100

    added tests

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='python-amazon-sp-api',
-    version='0.10.2',
+    version='0.10.3',
     install_requires=[
         "requests",
         "six>=1.15,<2",
