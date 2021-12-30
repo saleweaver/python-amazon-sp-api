4656c7b4dd21b94b9c48e1c3ff84412c4597d985
Michael <info@saleweaver.com>
2021-12-30 18:28:31 +0100

    updated listings_items, added tests for listings_items and product_type_definitions

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='python-amazon-sp-api',
-    version='0.11.1',
+    version='0.12.0',
     install_requires=[
         "requests",
         "six>=1.15,<2",
