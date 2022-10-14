daea97769d88ab623e1f4d479cffa4b4cc3b8da0
asoltaneioana <oana.asoltanei@sellerengine.com>
2022-10-14 12:54:16 +0200

    Don't double escape item IDs in product pricing requests

--- sp_api/api/products/products.py
+++ sp_api/api/products/products.py
@@ -239,8 +239,7 @@ class Products(Client):
 
     def _create_get_pricing_request(self, item_list, item_type, **kwargs):
         return self._request(kwargs.pop('path'),
-                             params={**{f"{item_type}s": ','.join(
-                                 [urllib.parse.quote_plus(s) for s in item_list])},
+                             params={**{f"{item_type}s": ','.join(item_list)},
                                      'ItemType': item_type,
                                      **({'ItemCondition': kwargs.pop(
                                          'ItemCondition')} if 'ItemCondition' in kwargs else {}),
