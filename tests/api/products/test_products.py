261246ec24ff2f2eb59682168c159f38b4195a01
Justin McDaniel <4643559+justinmcdaniel@users.noreply.github.com>
2023-07-31 17:23:20 -0500

    Added Products /batches/products/pricing/v0/listingOffers endpoint.

--- tests/api/products/test_products.py
+++ tests/api/products/test_products.py
@@ -1,4 +1,7 @@
+import traceback
+
 from sp_api.api.products.products import Products
+from sp_api.api.products.products_definitions import GetListingOffersBatchRequest, ListingOffersRequest
 from sp_api.base import ApiResponse, Marketplaces, SellingApiBadRequestException
 
 
@@ -30,7 +33,111 @@ def test_competitive_pricing_for_asin():
 
 
 def test_get_item_offers_batch():
-    res = Products().get_item_offers_batch([])
+    res = Products().get_item_offers_batch(requests_=[
+        {
+            "uri": "/products/pricing/v0/items/B000P6Q7MY/offers",
+            "method": "GET",
+            "MarketplaceId": "ATVPDKIKX0DER",
+            "ItemCondition": "New",
+            "CustomerType": "Consumer"
+        },
+        {
+            "uri": "/products/pricing/v0/items/B001Q3KU9Q/offers",
+            "method": "GET",
+            "MarketplaceId": "ATVPDKIKX0DER",
+            "ItemCondition": "New",
+            "CustomerType": "Consumer"
+        },
+        {
+            "uri": "/products/pricing/v0/items/B007Z07UK6/offers",
+            "method": "GET",
+            "MarketplaceId": "ATVPDKIKX0DER",
+            "ItemCondition": "New",
+            "CustomerType": "Consumer"
+        },
+        {
+            "uri": "/products/pricing/v0/items/B000OQA3N4/offers",
+            "method": "GET",
+            "MarketplaceId": "ATVPDKIKX0DER",
+            "ItemCondition": "New",
+            "CustomerType": "Consumer"
+        },
+        {
+            "uri": "/products/pricing/v0/items/B07PTMKYS7/offers",
+            "method": "GET",
+            "MarketplaceId": "ATVPDKIKX0DER",
+            "ItemCondition": "New",
+            "CustomerType": "Consumer"
+        },
+        {
+            "uri": "/products/pricing/v0/items/B001PYUTII/offers",
+            "method": "GET",
+            "MarketplaceId": "ATVPDKIKX0DER",
+            "ItemCondition": "New",
+            "CustomerType": "Consumer"
+        },
+        {
+            "uri": "/products/pricing/v0/items/B00505DW2I/offers",
+            "method": "GET",
+            "MarketplaceId": "ATVPDKIKX0DER",
+            "ItemCondition": "New",
+            "CustomerType": "Consumer"
+        },
+        {
+            "uri": "/products/pricing/v0/items/B00CGZQU42/offers",
+            "method": "GET",
+            "MarketplaceId": "ATVPDKIKX0DER",
+            "ItemCondition": "New",
+            "CustomerType": "Consumer"
+        },
+        {
+            "uri": "/products/pricing/v0/items/B01LY2ZYRF/offers",
+            "method": "GET",
+            "MarketplaceId": "ATVPDKIKX0DER",
+            "ItemCondition": "New",
+            "CustomerType": "Consumer"
+        },
+        {
+            "uri": "/products/pricing/v0/items/B00KFRNZY6/offers",
+            "method": "GET",
+            "MarketplaceId": "ATVPDKIKX0DER",
+            "ItemCondition": "New",
+            "CustomerType": "Consumer"
+        }
+    ])
     assert res.errors is None
     assert isinstance(res, ApiResponse)
 
+def test_get_listing_offers_batch():
+    reqs = [
+        ListingOffersRequest(
+            uri="/products/pricing/v0/listings/GC-QTMS-SV2I/offers",
+            MarketplaceId='ATVPDKIKX0DER',
+            ItemCondition='New'
+        ),
+        ListingOffersRequest(
+            uri="/products/pricing/v0/listings/VT-DEIT-57TQ/offers",
+            MarketplaceId='ATVPDKIKX0DER',
+            ItemCondition='New'
+        ),
+        ListingOffersRequest(
+            uri="/products/pricing/v0/listings/NA-H7X1-JYTM/offers",
+            MarketplaceId='ATVPDKIKX0DER',
+            ItemCondition='New'
+        ),
+        ListingOffersRequest(
+            uri="/products/pricing/v0/listings/RL-JVOC-MBSL/offers",
+            MarketplaceId='ATVPDKIKX0DER',
+            ItemCondition='New'
+        ),
+        ListingOffersRequest(
+            uri="/products/pricing/v0/listings/74-64KG-H9W9/offers",
+            MarketplaceId='ATVPDKIKX0DER',
+            ItemCondition='New'
+        ),
+    ]
+
+    batch_req = GetListingOffersBatchRequest(reqs)
+    res = Products().get_listing_offers_batch(batch_req)
+    assert res.errors is None
+    assert isinstance(res, ApiResponse)
