2fdd6a8342b1d6262af39b85667f42549219315d
Michael <info@saleweaver.com>
2023-10-15 12:22:57 +0200

    remove aws-sigv4

--- sp_api/base/__init__.py
+++ sp_api/base/__init__.py
@@ -1,4 +1,3 @@
-from .aws_sig_v4 import AWSSigV4
 from .base_client import BaseClient
 from .client import Client
 from .helpers import fill_query_params, sp_endpoint, create_md5, nest_dict, _nest_dict_rec, deprecated
@@ -38,7 +37,6 @@ __all__ = [
     'ApiResponse',
     'Client',
     'BaseClient',
-    'AWSSigV4',
     'Marketplaces',
     'fill_query_params',
     'sp_endpoint',
