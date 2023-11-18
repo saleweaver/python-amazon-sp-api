f4ca3fe9ae8568d106eca19a190a3670b8c5e650
saleweaver <info@saleweaver.com>
2023-11-18 01:00:10 +0000

    add documentation

--- sp_api/api/data_kiosk/data_kiosk.py
+++ sp_api/api/data_kiosk/data_kiosk.py
@@ -22,9 +22,12 @@ class DataKiosk(Client):
 
         **Usage Plan:**
 
-        | Rate (requests per second) | Burst |
-        | ---- | ---- |
-        | 0.0222 | 10 |
+        ======================================  ==============
+        Rate (requests per second)               Burst
+        ======================================  ==============
+        0.0222                                  10
+        ======================================  ==============
+
 
         The `x-amzn-RateLimit-Limit` response header returns the usage plan rate limits that were applied to the requested operation, when available. The table above indicates the default rate and burst values for this operation. Selling partners whose business demands require higher throughput may see higher rate and burst values than those shown here. For more information, refer to [Usage Plans and Rate Limits in the Selling Partner API](https://developer-docs.amazon.com/sp-api/docs/usage-plans-and-rate-limits-in-the-sp-api).
 
@@ -58,9 +61,12 @@ class DataKiosk(Client):
 
         **Usage Plan:**
 
-        | Rate (requests per second) | Burst |
-        | ---- | ---- |
-        | 0.0167 | 15 |
+        ======================================  ==============
+        Rate (requests per second)               Burst
+        ======================================  ==============
+        0.0167                                  15
+        ======================================  ==============
+
 
         The `x-amzn-RateLimit-Limit` response header returns the usage plan rate limits that were applied to the requested operation, when available. The table above indicates the default rate and burst values for this operation. Selling partners whose business demands require higher throughput may see higher rate and burst values than those shown here. For more information, refer to [Usage Plans and Rate Limits in the Selling Partner API](https://developer-docs.amazon.com/sp-api/docs/usage-plans-and-rate-limits-in-the-sp-api).
 
@@ -91,9 +97,11 @@ class DataKiosk(Client):
 
         **Usage Plan:**
 
-        | Rate (requests per second) | Burst |
-        | ---- | ---- |
-        | 0.0222 | 10 |
+        ======================================  ==============
+        Rate (requests per second)               Burst
+        ======================================  ==============
+        0.0222                                  10
+        ======================================  ==============
 
         The `x-amzn-RateLimit-Limit` response header returns the usage plan rate limits that were applied to the requested operation, when available. The table above indicates the default rate and burst values for this operation. Selling partners whose business demands require higher throughput may see higher rate and burst values than those shown here. For more information, refer to [Usage Plans and Rate Limits in the Selling Partner API](https://developer-docs.amazon.com/sp-api/docs/usage-plans-and-rate-limits-in-the-sp-api).
 
@@ -117,9 +125,11 @@ class DataKiosk(Client):
 
         **Usage Plan:**
 
-        | Rate (requests per second) | Burst |
-        | ---- | ---- |
-        | 2.0 | 15 |
+        ======================================  ==============
+        Rate (requests per second)               Burst
+        ======================================  ==============
+        2                                       15
+        ======================================  ==============
 
         The `x-amzn-RateLimit-Limit` response header returns the usage plan rate limits that were applied to the requested operation, when available. The table above indicates the default rate and burst values for this operation. Selling partners whose business demands require higher throughput may see higher rate and burst values than those shown here. For more information, refer to [Usage Plans and Rate Limits in the Selling Partner API](https://developer-docs.amazon.com/sp-api/docs/usage-plans-and-rate-limits-in-the-sp-api).
 
@@ -143,9 +153,11 @@ class DataKiosk(Client):
 
         **Usage Plan:**
 
-        | Rate (requests per second) | Burst |
-        | ---- | ---- |
-        | 0.0167 | 15 |
+        ======================================  ==============
+        Rate (requests per second)               Burst
+        ======================================  ==============
+        0.0167                                  15
+        ======================================  ==============
 
         The `x-amzn-RateLimit-Limit` response header returns the usage plan rate limits that were applied to the requested operation, when available. The table above indicates the default rate and burst values for this operation. Selling partners whose business demands require higher throughput may see higher rate and burst values than those shown here. For more information, refer to [Usage Plans and Rate Limits in the Selling Partner API](https://developer-docs.amazon.com/sp-api/docs/usage-plans-and-rate-limits-in-the-sp-api).
 
